0972de265730b12d6a9829618d7de5dde637c195
I Nyoman Pastika <pastikaerror2625@gmail.com>
2026-02-13 22:14:21 +0800

    update url api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5004/api"
+  baseURL: "https://api.peltidenpasar.org"
 });
 
 api.interceptors.request.use((config) => {
