4c21181b634e6aac2a8aaa35d481cdf9b222bb9f
I Nyoman Pastika <pastikaerror2625@gmail.com>
2026-05-29 23:20:33 +0800

    update path origin index.js

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ app.use(cors({
     "https://www.peltidenpasar.org",
     "http://localhost:5173"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
